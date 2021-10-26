4fe706fc6270481092bb7745b421abfec3996d76
Kedar Wadhavkar <67806501+Kedar-Wadhavkar@users.noreply.github.com>
2021-10-26 19:08:40 +0530

    Update list.js

--- js/list.js
+++ js/list.js
@@ -9,12 +9,16 @@ const searchFun = () =>{
 	for(var i=0; i<tr.length; i++){
 	
 		let td = tr[i].getElementsByTagName('td')[0];
+		let td2 = tr[i].getElementsByTagName('td')[1];
 
-		if(td){
-
+		if(td||td2){
 			let textValue = td.textContent || td.innerHTML;
+			let textValue2 = td2.textContent || td2.innerHTML;
 
 			if(textValue.indexOf(filter) > -1){
+				tr[i].style.display = "";
+			}
+			else if(textValue2.indexOf(filter) > -1){
 				tr[i].style.display = "";
 			}
 			else{
