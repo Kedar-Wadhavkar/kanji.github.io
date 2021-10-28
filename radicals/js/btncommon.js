b3ed74006799d28a6d690c14df426d131c3abb81
Kedar Wadhavkar <67806501+Kedar-Wadhavkar@users.noreply.github.com>
2021-10-29 01:43:25 +0530

    Big Update

---
+++ radicals/js/btncommon.js
@@ -0,0 +1,8 @@
+function down(btn) {	
+	btn.style.color = "#2dcac2";
+	btn.style.backgroundColor = "#121212";
+}
+function up(btn) {
+	btn.style.color = "#121212";
+	btn.style.backgroundColor = "#2dcac2";
+}
