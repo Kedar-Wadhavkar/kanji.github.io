1e6c76f30a2a7753f82fc370d618dea63ce3aac0
Kedar Wadhavkar <67806501+Kedar-Wadhavkar@users.noreply.github.com>
2021-10-29 01:00:57 +0530

    Update

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const down1 = () =>{
+	jQuery("#btn1").css("background-color", "#121212");
+	jQuery("#btn1").css("color", "#2dcac2");
+}
+const up1 = () =>{
+	jQuery("#btn1").css("background-color", "#2dcac2");
+	jQuery("#btn1").css("color", "#121212");
+}
+
+
+const down2 = () =>{
+	jQuery("#btn2").css("background-color", "#121212");
+	jQuery("#btn2").css("color", "#2dcac2");
+}
+const up2 = () =>{
+	jQuery("#btn2").css("background-color", "#2dcac2");
+	jQuery("#btn2").css("color", "#121212");
+}
+
+
+const down3 = () =>{
+	jQuery("#btn3").css("background-color", "#121212");
+	jQuery("#btn3").css("color", "#2dcac2");
+}
+const up3 = () =>{
+	jQuery("#btn3").css("background-color", "#2dcac2");
+	jQuery("#btn3").css("color", "#121212");
+}
