8b6cc8bc00b95958a463e4533e4197ba57163275
Kedar Wadhavkar <67806501+Kedar-Wadhavkar@users.noreply.github.com>
2021-11-01 02:50:31 +0530

    Update

---
+++ js/levels.js
@@ -0,0 +1,83 @@
+var links1 = document.getElementById( "a1" );
+
+  links1.addEventListener( "touchstart", function() {
+  	document.documentElement.style.setProperty('--txt1', "#fff")
+  	document.documentElement.style.setProperty('--bck1', "rgba(255,255,255,0.2)")
+  	document.documentElement.style.setProperty('--bdrbck1', "rgba(255,255,255,0.2)")
+
+	setTimeout(() => {
+  	document.documentElement.style.setProperty('--txt1', "#000")
+  	document.documentElement.style.setProperty('--bck1', "rgba(255,255,255,0.9)")
+  	document.documentElement.style.setProperty('--bdrbck1', "rgba(255,255,255,0.9)")
+	}, 500);
+  	});
+
+  links1.addEventListener( "touchend", function() {
+  	document.documentElement.style.setProperty('--txt1', "#000")
+  	document.documentElement.style.setProperty('--bck1', "rgba(255,255,255,0.9)")
+  	document.documentElement.style.setProperty('--bdrbck1', "rgba(255,255,255,0.9)")
+	});
+
+
+var links2 = document.getElementById( "a2" );
+
+  links2.addEventListener( "touchstart", function() {
+  	document.documentElement.style.setProperty('--txt2', "#fff")
+  	document.documentElement.style.setProperty('--bck2', "rgba(255,255,255,0.2)")
+  	document.documentElement.style.setProperty('--bdrbck2', "rgba(255,255,255,0.2)")
+
+	setTimeout(() => {
+  	document.documentElement.style.setProperty('--txt2', "#000")
+  	document.documentElement.style.setProperty('--bck2', "rgba(255,255,255,0.9)")
+  	document.documentElement.style.setProperty('--bdrbck2', "rgba(255,255,255,0.9)")
+	}, 500);
+  	});
+
+  links2.addEventListener( "touchend", function() {
+  	document.documentElement.style.setProperty('--txt2', "#000")
+  	document.documentElement.style.setProperty('--bck2', "rgba(255,255,255,0.9)")
+  	document.documentElement.style.setProperty('--bdrbck2', "rgba(255,255,255,0.9)")
+	});
+
+
+var links3 = document.getElementById( "a3" );
+
+  links3.addEventListener( "touchstart", function() {
+  	document.documentElement.style.setProperty('--txt3', "#fff")
+  	document.documentElement.style.setProperty('--bck3', "rgba(255,255,255,0.2)")
+  	document.documentElement.style.setProperty('--bdrbck3', "rgba(255,255,255,0.2)")
+
+	setTimeout(() => {
+  	document.documentElement.style.setProperty('--txt3', "#000")
+  	document.documentElement.style.setProperty('--bck3', "rgba(255,255,255,0.9)")
+  	document.documentElement.style.setProperty('--bdrbck3', "rgba(255,255,255,0.9)")
+	}, 500);
+  	});
+
+  links3.addEventListener( "touchend", function() {
+  	document.documentElement.style.setProperty('--txt3', "#000")
+  	document.documentElement.style.setProperty('--bck3', "rgba(255,255,255,0.9)")
+  	document.documentElement.style.setProperty('--bdrbck3', "rgba(255,255,255,0.9)")
+	});
+
+
+var links4 = document.getElementById( "a4" );
+
+  links4.addEventListener( "touchstart", function() {
+  	document.documentElement.style.setProperty('--txt4', "#fff")
+  	document.documentElement.style.setProperty('--bck4', "rgba(255,255,255,0.2)")
+  	document.documentElement.style.setProperty('--bdrbck4', "rgba(255,255,255,0.2)")
+
+	setTimeout(() => {
+  	document.documentElement.style.setProperty('--txt4', "#000")
+  	document.documentElement.style.setProperty('--bck4', "rgba(255,255,255,0.9)")
+  	document.documentElement.style.setProperty('--bdrbck4', "rgba(255,255,255,0.9)")
+	}, 500);
+  	});
+
+  links4.addEventListener( "touchend", function() {
+  	document.documentElement.style.setProperty('--txt4', "#000")
+  	document.documentElement.style.setProperty('--bck4', "rgba(255,255,255,0.9)")
+  	document.documentElement.style.setProperty('--bdrbck4', "rgba(255,255,255,0.9)")
+	});
+
