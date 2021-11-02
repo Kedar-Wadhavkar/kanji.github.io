78c3991850caed49124cff76a315e84185c74ca2
Kedar Wadhavkar <67806501+Kedar-Wadhavkar@users.noreply.github.com>
2021-11-03 02:11:50 +0530

    Update

--- radicals/js/radicalscommon.js
+++ radicals/js/radicalscommon.js
@@ -1,17 +1,14 @@
 function sel(selbck) {	
 	selbck.style.backgroundColor = "rgba(255,255,255,0.2)";	
 	selbck.style.color = "#fff";	
-	selbck.style.fontWeight = "bold";
 
 	setTimeout(() => {
 		selbck.style.color = "#000";
 		selbck.style.backgroundColor = "rgba(255,255,255,0.9)";
-		selbck.style.fontWeight = "normal";
 	}, 500);
 }
 
 function unsel(selbck) {
 		selbck.style.color = "#000";
 		selbck.style.backgroundColor = "rgba(255,255,255,0.9)";
-		selbck.style.fontWeight = "normal";
 }
