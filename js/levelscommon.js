04f92834decbc32719d884e2a1244e088b8e11f4
Kedar Wadhavkar <67806501+Kedar-Wadhavkar@users.noreply.github.com>
2021-11-04 10:23:35 +0530

    update

--- js/levelscommon.js
+++ js/levelscommon.js
@@ -1,14 +1,14 @@
 function sel(selbck) {			
 	selbck.style.color = "#2dcac2";	
-	selbck.style.boxShadow = "inset -1px -1px 2px rgba(45,202,194,0.8),inset 3px 3px 6px rgba(0,0,0,1)";
+	selbck.style.boxShadow = "inset -2px -2px 3px #aaa,inset 3px 3px 6px rgba(0,0,0,1)";
 
 	setTimeout(() => {
 		selbck.style.color = "#fff";
-	selbck.style.boxShadow = "-1px -1px 2px rgba(45,202,194),3px 3px 6px rgba(0,0,0,1)";
+	selbck.style.boxShadow = "-2px -2px 3px rgba(45,202,194),3px 3px 6px rgba(0,0,0,1)";
 	}, 500);
 }
 
 function unsel(selbck) {
 		selbck.style.color = "#fff";
-	selbck.style.boxShadow = "-1px -1px 2px rgba(45,202,194),3px 3px 6px rgba(0,0,0,1)";
+	selbck.style.boxShadow = "-2px -2px 3px rgba(45,202,194),3px 3px 6px rgba(0,0,0,1)";
 }
