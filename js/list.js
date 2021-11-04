9cb429023a0bd6d059e8d647ba7b95f8f8a83aad
Kedar Wadhavkar <67806501+Kedar-Wadhavkar@users.noreply.github.com>
2021-11-04 13:28:02 +0530

    update

--- js/list.js
+++ js/list.js
@@ -37,11 +37,13 @@ const searchFun = () =>{
 
 function sel(selbck) {			
 	selbck.style.color = "#2dcac2";	
+	selbck.style.fontWeight = "bold";	
 	selbck.style.backgroundColor = "#2d2d2d";	
 	selbck.style.boxShadow = "inset -2px -2px 3px rgba(45,202,194,0.75),inset 3px 3px 6px rgba(0,0,0,1)";
 
 	setTimeout(() => {
 		selbck.style.color = "#fff";
+	selbck.style.fontWeight = "normal";
 	selbck.style.backgroundColor = "#1f1f1f";	
 	selbck.style.boxShadow = "-2px -2px 3px rgba(45,202,194),3px 3px 6px rgba(0,0,0,1)";
 	}, 500);
@@ -49,6 +51,7 @@ function sel(selbck) {
 
 function unsel(selbck) {
 		selbck.style.color = "#fff";
+	selbck.style.fontWeight = "normal";
 	selbck.style.backgroundColor = "#1f1f1f";	
 	selbck.style.boxShadow = "-2px -2px 3px rgba(45,202,194),3px 3px 6px rgba(0,0,0,1)";
 }	
