2abca912a17f138bd5541194a86e4cedc0848a8d
Kedar Wadhavkar <67806501+Kedar-Wadhavkar@users.noreply.github.com>
2021-11-04 12:32:11 +0530

    update

--- js/list.js
+++ js/list.js
@@ -10,15 +10,20 @@ const searchFun = () =>{
 	
 		let td = tr[i].getElementsByTagName('td')[0];
 		let td2 = tr[i].getElementsByTagName('td')[1];
+		let td3 = tr[i].getElementsByTagName('td')[2];
 
 		if(td||td2){
 			let textValue = td.textContent || td.innerHTML;
 			let textValue2 = td2.textContent || td2.innerHTML;
+			let textValue3 = td3.textContent || td3.innerHTML;
 
 			if(textValue.indexOf(filter) > -1){
 				tr[i].style.display = "";
 			}
 			else if(textValue2.toUpperCase().indexOf(filter) > -1){
+				tr[i].style.display = "";
+			}
+			else if(textValue3.toUpperCase().indexOf(filter) > -1){
 				tr[i].style.display = "";
 			}
 			else{
@@ -30,20 +35,21 @@ const searchFun = () =>{
 
 
 
-function sel(selbck) {		
-	selbck.style.opacity = "1";
-	selbck.style.backgroundColor = "rgba(255,255,255,0.2)";
-	selbck.style.borderRadius = "8px";
-	selbck.style.color = "#fff"
+function sel(selbck) {			
+	selbck.style.color = "#2dcac2";	
+	selbck.style.backgroundColor = "#2d2d2d";	
+	selbck.style.boxShadow = "inset -2px -2px 3px rgba(45,202,194,0.75),inset 3px 3px 6px rgba(0,0,0,1)";
 
 	setTimeout(() => {
-		selbck.style.backgroundColor = "rgba(255,255,255,0.9)";
-		selbck.style.color = "#000";
+		selbck.style.color = "#fff";
+	selbck.style.backgroundColor = "#1f1f1f";	
+	selbck.style.boxShadow = "-2px -2px 3px rgba(45,202,194),3px 3px 6px rgba(0,0,0,1)";
 	}, 500);
 }
 
-function unsel(selbck) {	
-	selbck.style.backgroundColor = "rgba(255,255,255,0.9)";
-	selbck.style.color = "#000";
+function unsel(selbck) {
+		selbck.style.color = "#fff";
+	selbck.style.backgroundColor = "#1f1f1f";	
+	selbck.style.boxShadow = "-2px -2px 3px rgba(45,202,194),3px 3px 6px rgba(0,0,0,1)";
 }	
 
