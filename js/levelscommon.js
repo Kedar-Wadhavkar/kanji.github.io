8673376eb1210327d03a79ceb8798f6168aca459
Kedar Wadhavkar <67806501+Kedar-Wadhavkar@users.noreply.github.com>
2021-11-04 11:02:26 +0530

    update

--- js/levelscommon.js
+++ js/levelscommon.js
@@ -1,6 +1,6 @@
 function sel(selbck) {			
 	selbck.style.color = "#2dcac2";	
-	selbck.style.boxShadow = "inset -2px -2px 3px #aaa,inset 3px 3px 6px rgba(0,0,0,1)";
+	selbck.style.boxShadow = "inset -2px -2px 3px rgba(45,202,194,0.75),inset 3px 3px 6px rgba(0,0,0,1)";
 
 	setTimeout(() => {
 		selbck.style.color = "#fff";
